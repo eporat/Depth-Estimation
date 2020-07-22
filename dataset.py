57c415f94aa8eef1c73d52b7d97b9429e305c830
eporat <ethan.porat@gmail.com>
2020-07-22 18:43:58 +0300

    data loader

--- dataset.py
+++ dataset.py
@@ -2,16 +2,18 @@ from __future__ import print_function, division
 import os
 import torch
 import pandas as pd
-from skimage import io, transform
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
+from load_data import load_pfm
+from skimage import io
+from utils import *
 
-class FaceLandmarksDataset(Dataset):
-    """Face Landmarks dataset."""
+class DepthMapDataset(Dataset):
+    """Depth Map dataset."""
 
-    def __init__(self, csv_file, root_dir, transform=None):
+    def __init__(self, dataset, transform=None):
         """
         Args:
             csv_file (string): Path to the csv file with annotations.
@@ -19,26 +21,38 @@ class FaceLandmarksDataset(Dataset):
             transform (callable, optional): Optional transform to be applied
                 on a sample.
         """
-        self.landmarks_frame = pd.read_csv(csv_file)
-        self.root_dir = root_dir
+        self.dataset = dataset
         self.transform = transform
+        self.folders = os.listdir(dataset)
 
     def __len__(self):
-        return len(self.landmarks_frame)
+        return len(self.folders)
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        img_name = os.path.join(self.root_dir,
-                                self.landmarks_frame.iloc[idx, 0])
-        image = io.imread(img_name)
-        landmarks = self.landmarks_frame.iloc[idx, 1:]
-        landmarks = np.array([landmarks])
-        landmarks = landmarks.astype('float').reshape(-1, 2)
-        sample = {'image': image, 'landmarks': landmarks}
+        folder = self.folders[idx]
+        image_path = f'{self.dataset}/{folder}/cyl/photo_crop.jpg'
+        depth_map_path = f'{self.dataset}/{folder}/cyl/distance_crop.pfm.gz'
+        gunzip_shutil(depth_map_path, 'temp/file.pfm')
+        depth_map, scale = load_pfm(open('temp/file.pfm', 'rb'))
+        depth_map = depth_map[::-1]
+        image = io.imread(image_path)
+
+        sample = {'image': image, 'depth_map': depth_map, 'scale': scale}
 
         if self.transform:
             sample = self.transform(sample)
 
         return sample
+
+class DepthMapDataLoader(DataLoader):
+    def __init__(self, dataset, batch_size=4, shuffle=True):
+        DataLoader.__init__(self, dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn)
+
+def collate_fn(batch):
+    images = [item['image'] for item in batch]
+    depth_maps = [item['depth_map'] for item in batch]
+    return [images, depth_maps]
+            
--- main.py
+++ main.py
@@ -2,30 +2,18 @@ from load_data import load_pfm
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-from os.path import join
-from imageio import imread
-import tarfile
-from load_data import load_pfm
-import gzip
-import shutil
+from dataset import DepthMapDataset, DepthMapDataLoader
 
-dataset = "C:\\Users\\ethan\\Downloads\\geoPose3K_final_publish\\geoPose3K_final_publish"
+dataset = DepthMapDataset("C:\\Users\\ethan\\Downloads\\geoPose3K_final_publish\\geoPose3K_final_publish")
+dataloader = DepthMapDataLoader(dataset=dataset)
 
-def gunzip_shutil(source_filepath, dest_filepath, block_size=65536):
-    with gzip.open(source_filepath, 'rb') as s_file, \
-            open(dest_filepath, 'wb') as d_file:
-        shutil.copyfileobj(s_file, d_file, block_size)
-
-
-for folder in os.listdir(dataset):
-    image_path = f'{dataset}/{folder}/cyl/photo_crop.jpg'
-    depth_map_path = f'{dataset}/{folder}/cyl/distance_crop.pfm.gz'
-    gunzip_shutil(depth_map_path, 'temp/file.pfm')
-    depth_map, scale = load_pfm(open('temp/file.pfm', 'rb'))
-    depth_map = depth_map[::-1]
-    image = imread(image_path)
-    plt.figure(1)
-    plt.imshow(depth_map)
-    plt.figure(2)
-    plt.imshow(image)
-    plt.show()
+for i_batch, sample_batched in enumerate(dataloader):
+    images, depth_maps = sample_batched
+    
+    for image, depth_map in zip(images, depth_maps):
+        plt.figure(1)
+        plt.imshow(image)
+        plt.figure(2)
+        plt.imshow(depth_map)
+        plt.show()
+        
